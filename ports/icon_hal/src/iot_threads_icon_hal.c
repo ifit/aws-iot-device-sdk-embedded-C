0e02d83a04580bc46218ff6c9dbe7b958d2a8fdb
Kade Cox <kade.cox@iconfitness.com>
2020-07-06 09:36:21 -0600

    Finished the thread and semaphore port

--- ports/icon_hal/src/iot_threads_icon_hal.c
+++ ports/icon_hal/src/iot_threads_icon_hal.c
@@ -98,6 +98,12 @@
 #define IOT_THREAD_TABLE_INIT_ELEMENT(X) \
 { .threadRoutine=NULL, .pArgument = NULL, .priority = 0, .stackSize=0, .name= IOT_THREAD_NAME(X) }
 
+#ifdef CONFIG_DEBUG_AWS_PORT_LAYER
+#define DEBUG_MSG(...) IH_PRINT_DEBUG_MESSAGE(__VA_ARGS__)
+#else
+#define DEBUG_MSG(...)
+#endif //CONFIG_DEBUG_AWS_PORT_LAYER
+
 /***********************************************************************************/
 /***************************** Type Defs *******************************************/
 /***********************************************************************************/
@@ -118,6 +124,19 @@ struct iot_fifo_cleanup_data_s
 {
     struct iot_thread_data_entry_s *entry;  //!< Pointer to the entry to clean up
 }; //!< Structure used with the cleanup function called by the HAL Fifo task
+
+struct iot_mutex_data_s
+{
+    SemaphoreHandle_t handle; //!< Handle for freertos mutex
+    StaticSemaphore_t mutex_data; //!< Freertos Mutex data
+    bool recursive; //!< Tells if it is a recursive mutex
+}; //!< Structure that holds the data for a mutex for this IOT port
+
+struct iot_semaphore_data_s
+{
+    SemaphoreHandle_t handle; //!< Handle for freertos mutex
+    StaticSemaphore_t semaphore_data; //!< Freertos Semaphore data
+}; //!< Data for a counting semaphore
 
 /***********************************************************************************/
 /***************************** Function Declarations *******************************/
@@ -268,6 +287,7 @@ bool Iot_CreateDetachedThread(IotThreadRoutine_t threadRoutine,
 
     if(NULL == threadRoutine || 150 > stackSize || tskIDLE_PRIORITY > priority ||  configMAX_PRIORITIES < priority)
     {
+        DEBUG_MSG("Invalid Params\r\n");
         return false;
     }
 
@@ -283,6 +303,7 @@ bool Iot_CreateDetachedThread(IotThreadRoutine_t threadRoutine,
     IOT_THREAD_EXIT_CRITICAL()
     if(NULL == table_entry)
     {
+        DEBUG_MSG("Failed to fetch entry\r\n");
         return false;
     }
 
@@ -295,6 +316,7 @@ bool Iot_CreateDetachedThread(IotThreadRoutine_t threadRoutine,
         table_entry->priority = 0;
         table_entry->stackSize = 0;
         IOT_THREAD_EXIT_CRITICAL()
+        DEBUG_MSG("Malloc Failed\r\n");
         return false;
     }
 
@@ -308,6 +330,7 @@ bool Iot_CreateDetachedThread(IotThreadRoutine_t threadRoutine,
         table_entry->priority = 0;
         table_entry->stackSize = 0;
         IOT_THREAD_EXIT_CRITICAL()
+        DEBUG_MSG("xTaskCreateStatic Failed\r\n");
         return false;
     }
 
@@ -348,48 +371,215 @@ bool IotMutex_Create(IotMutex_t *pNewMutex, bool recursive)
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_mutexcreate.html
      */
-    return false;
+    struct iot_mutex_data_s *new_mutex;
+
+    new_mutex = iot_port_malloc(sizeof(struct iot_mutex_data_s));
+
+    if(NULL == new_mutex)
+    {
+        //Failed to allocate data for the mutex
+        return false;
+    }
+
+    new_mutex->handle = NULL;
+    if(false == recursive)
+    {
+        new_mutex->handle = xSemaphoreCreateMutexStatic(&new_mutex->mutex_data);
+        new_mutex->recursive = false;
+    }
+    else
+    {
+        new_mutex->handle = xSemaphoreCreateRecursiveMutexStatic(&new_mutex->mutex_data);
+        new_mutex->recursive = true;
+    }
+    if(NULL == new_mutex->handle)
+    {
+        //Failed to create the mutex
+        iot_port_free(new_mutex);
+        return false;
+    }
+
+    pNewMutex->pointer = new_mutex;
+
+    DEBUG_MSG("+++Created IOT MUTEX %p\r\n", pNewMutex->pointer);
+    return true;
 }
 
-/*-----------------------------------------------------------*/
 
+/**
+ * @brief Free resources used by a mutex.
+ *
+ * This function frees resources used by a mutex. It must be called on an initialized
+ * #IotMutex_t. No other mutex functions should be called on `pMutex` after calling
+ * this function (unless the mutex is re-created).
+ *
+ * @param[in] pMutex The mutex to destroy.
+ *
+ * @warning This function must not be called on a locked mutex.
+ * @see @ref platform_threads_function_mutexcreate
+ */
 void IotMutex_Destroy(IotMutex_t *pMutex)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_mutexdestroy.html
      */
+    struct iot_mutex_data_s *typed;
+
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pMutex);
+    typed = pMutex->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+
+    DEBUG_MSG("---Destroing IOT Mutex at %p\r\n", typed);
+    vSemaphoreDelete(typed->handle);
+
+    iot_port_free(typed);
 }
 
-/*-----------------------------------------------------------*/
 
+/**
+ * @brief Lock a mutex. This function should only return when the mutex is locked;
+ * it is not expected to fail.
+ *
+ * This function blocks and waits until a mutex is available. It waits forever
+ * (deadlocks) if `pMutex` is already locked and never unlocked.
+ *
+ * @param[in] pMutex The mutex to lock.
+ *
+ * @see @ref platform_threads_function_mutextrylock for a nonblocking lock.
+ */
 void IotMutex_Lock(IotMutex_t *pMutex)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_mutexlock.html
      */
+    struct iot_mutex_data_s *typed;
+
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, false == ih_in_interrupt());
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pMutex);
+    typed = pMutex->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    DEBUG_MSG("-m-m-m %s using IOT Mutex at %p\r\n", __FUNCTION__, typed);
+
+    if(true == typed->recursive)
+    {
+        IH_ASSERT(IH_ERR_LEVEL_ERROR, pdTRUE == xSemaphoreTakeRecursive(typed->handle, portMAX_DELAY));
+    }
+    else
+    {
+        IH_ASSERT(IH_ERR_LEVEL_ERROR, pdTRUE == xSemaphoreTake(typed->handle, portMAX_DELAY));
+    }
 }
 
-/*-----------------------------------------------------------*/
 
+/**
+ * @brief Lock a mutex. This function should only return when the mutex is locked;
+ * it is not expected to fail.
+ *
+ * This function blocks and waits until a mutex is available. It waits forever
+ * (deadlocks) if `pMutex` is already locked and never unlocked.
+ *
+ * @param[in] pMutex The mutex to lock.
+ *
+ * @see @ref platform_threads_function_mutextrylock for a nonblocking lock.
+ */
 bool IotMutex_TryLock(IotMutex_t *pMutex)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_mutextrylock.html
      */
+
+    struct iot_mutex_data_s *typed;
+    BaseType_t result;
+
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, false == ih_in_interrupt());
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pMutex);
+    typed = pMutex->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    DEBUG_MSG("-m-m-m %s using IOT Mutex at %p\r\n", __FUNCTION__, typed);
+
+    if(true == typed->recursive)
+    {
+        result = xSemaphoreTakeRecursive(typed->handle, 0);
+    }
+    else
+    {
+        result = xSemaphoreTake(typed->handle, 0);
+    }
+
+    if(pdTRUE == result)
+    {
+        return true;
+    }
     return false;
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Unlock a mutex. This function should only return when the mutex is unlocked;
+ * it is not expected to fail.
+ *
+ * Unlocks a locked mutex. `pMutex` must have been locked by the thread calling
+ * this function.
+ *
+ * @param[in] pMutex The mutex to unlock.
+ *
+ * @note This function should not be called on a mutex that is already unlocked.
+ */
 void IotMutex_Unlock(IotMutex_t *pMutex)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_mutexunlock.html
      */
+    struct iot_mutex_data_s *typed;
+
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, false == ih_in_interrupt());
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pMutex);
+    typed = pMutex->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    DEBUG_MSG("-m-m-m %s using IOT Mutex at %p\r\n", __FUNCTION__, typed);
+
+    if(true == typed->recursive)
+    {
+        IH_ASSERT(IH_ERR_LEVEL_ERROR, pdTRUE == xSemaphoreGiveRecursive(typed->handle));
+    }
+    else
+    {
+        IH_ASSERT(IH_ERR_LEVEL_ERROR, pdTRUE == xSemaphoreGive(typed->handle));
+    }
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Create a new counting semaphore.
+ *
+ * This function creates a new counting semaphore with a given initial and
+ * maximum value. It must be called on an uninitialized #IotSemaphore_t.
+ * This function must not be called on an already-initialized #IotSemaphore_t.
+ *
+ * @param[in] pNewSemaphore Pointer to the memory that will hold the new semaphore.
+ * @param[in] initialValue The semaphore should be initialized with this value.
+ * @param[in] maxValue The maximum value the semaphore will reach.
+ *
+ * @return `true` if semaphore creation succeeds; `false` otherwise.
+ *
+ * @see @ref platform_threads_function_semaphoredestroy
+ *
+ * <b>Example</b>
+ * @code{c}
+ * IotSemaphore_t sem;
+ *
+ * // Create a locked binary semaphore.
+ * if( IotSemaphore_Create( &sem, 0, 1 ) == true )
+ * {
+ *     // Unlock the semaphore.
+ *     IotSemaphore_Post( &sem );
+ *
+ *     // Destroy the semaphore when it's no longer needed.
+ *     IotSemaphore_Destroy( &sem );
+ * }
+ * @endcode
+ */
 bool IotSemaphore_Create(IotSemaphore_t *pNewSemaphore,
                          uint32_t initialValue,
                          uint32_t maxValue)
@@ -397,65 +587,210 @@ bool IotSemaphore_Create(IotSemaphore_t *pNewSemaphore,
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_semaphorecreate.html
      */
-    return false;
+    struct iot_semaphore_data_s *worker;
+    worker = iot_port_malloc(sizeof(struct iot_semaphore_data_s));
+
+    if(NULL == worker)
+    {
+        return false;
+    }
+
+    worker->handle = xSemaphoreCreateCountingStatic(maxValue, initialValue, &worker->semaphore_data);
+
+    if(NULL == worker->handle)
+    {
+        //Failed to create the static counting semaphore
+        iot_port_free(worker);
+        return false;
+    }
+
+    pNewSemaphore->pointer = worker;
+    DEBUG_MSG("+++Created IOT Semaphore at %p\r\n", pNewSemaphore->pointer);
+    return true;
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Free resources used by a semaphore.
+ *
+ * This function frees resources used by a semaphore. It must be called on an initialized
+ * #IotSemaphore_t. No other semaphore functions should be called on `pSemaphore` after
+ * calling this function (unless the semaphore is re-created).
+ *
+ * @param[in] pSemaphore The semaphore to destroy.
+ *
+ * @warning This function must not be called on a semaphore with waiting threads.
+ * @see @ref platform_threads_function_semaphorecreate
+ */
 void IotSemaphore_Destroy(IotSemaphore_t *pSemaphore)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_semaphoredestroy.html
      */
+    struct iot_semaphore_data_s *typed;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pSemaphore);
+    typed = pSemaphore->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    DEBUG_MSG("---Destroying IOT Semaphore at %p\r\n", typed);
+    vSemaphoreDelete(typed->handle);
+
+    iot_port_free(typed);
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Query the current count of the semaphore.
+ *
+ * This function queries a counting semaphore for its current value. A counting
+ * semaphore's value is always 0 or positive.
+ *
+ * @param[in] pSemaphore The semaphore to query.
+ *
+ * @return The current count of the semaphore. This function should not fail.
+ */
 uint32_t IotSemaphore_GetCount(IotSemaphore_t *pSemaphore)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_semaphoregetcount.html
      */
-    return 0;
+    struct iot_semaphore_data_s *typed;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pSemaphore);
+    typed = pSemaphore->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    DEBUG_MSG("-s-s-s %s using semaphore at %p\r\n", __FUNCTION__, typed);
+    return uxSemaphoreGetCount(typed->handle);
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Wait on (lock) a semaphore. This function should only return when the
+ * semaphore wait succeeds; it is not expected to fail.
+ *
+ * This function blocks and waits until a counting semaphore is positive. It
+ * waits forever (deadlocks) if `pSemaphore` has a count `0` that is never
+ * [incremented](@ref platform_threads_function_semaphorepost).
+ *
+ * @param[in] pSemaphore The semaphore to lock.
+ *
+ * @see @ref platform_threads_function_semaphoretrywait for a nonblocking wait;
+ * @ref platform_threads_function_semaphoretimedwait for a wait with timeout.
+ */
 void IotSemaphore_Wait(IotSemaphore_t *pSemaphore)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_semaphorewait.html
      */
+    struct iot_semaphore_data_s *typed;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pSemaphore);
+    typed = pSemaphore->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, false == ih_in_interrupt());
+
+    DEBUG_MSG("-s-s-s %s using semaphore at %p\r\n", __FUNCTION__, typed);
+
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, pdTRUE == xSemaphoreTake(typed->handle, portMAX_DELAY));
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Attempt to wait on (lock) a semaphore. Return immediately if the semaphore
+ * is not available.
+ *
+ * If the count of `pSemaphore` is positive, this function immediately decrements
+ * the semaphore and returns. Otherwise, this function returns without decrementing
+ * `pSemaphore`.
+ *
+ * @param[in] pSemaphore The semaphore to lock.
+ *
+ * @return `true` if the semaphore wait succeeded; `false` if the semaphore has
+ * a count of `0`.
+ *
+ * @see @ref platform_threads_function_semaphorewait for a blocking wait;
+ * @ref platform_threads_function_semaphoretimedwait for a wait with timeout.
+ */
 bool IotSemaphore_TryWait(IotSemaphore_t *pSemaphore)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_semaphoretrywait.html
      */
+    struct iot_semaphore_data_s *typed;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pSemaphore);
+    typed = pSemaphore->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, false == ih_in_interrupt());
+
+    DEBUG_MSG("-s-s-s %s using semaphore at %p\r\n", __FUNCTION__, typed);
+
+    if(pdTRUE == xSemaphoreTake(typed->handle, 0))
+    {
+        return true;
+    }
     return false;
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Attempt to wait on (lock) a semaphore with a timeout.
+ *
+ * This function blocks and waits until a counting semaphore is positive
+ * <i>or</i> its timeout expires (whichever is sooner). It decrements
+ * `pSemaphore` and returns `true` if the semaphore is positive at some
+ * time during the wait. If `pSemaphore` is always `0` during the wait,
+ * this function returns `false`.
+ *
+ * @param[in] pSemaphore The semaphore to lock.
+ * @param[in] timeoutMs Relative timeout of semaphore lock. This function returns
+ * false if the semaphore couldn't be locked within this timeout.
+ *
+ * @return `true` if the semaphore wait succeeded; `false` if it timed out.
+ *
+ * @see @ref platform_threads_function_semaphoretrywait for a nonblocking wait;
+ * @ref platform_threads_function_semaphorewait for a blocking wait.
+ */
 bool IotSemaphore_TimedWait(IotSemaphore_t *pSemaphore,
                             uint32_t timeoutMs)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_semaphoretimedwait.html
      */
+    struct iot_semaphore_data_s *typed;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pSemaphore);
+    typed = pSemaphore->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, false == ih_in_interrupt());
+
+    DEBUG_MSG("-s-s-s %s using semaphore at %p\r\n", __FUNCTION__, typed);
+
+    if(pdTRUE == xSemaphoreTake(typed->handle, pdMS_TO_TICKS(timeoutMs)))
+    {
+        return true;
+    }
     return false;
 }
 
-/*-----------------------------------------------------------*/
-
+/**
+ * @brief Post to (unlock) a semaphore. This function should only return when the
+ * semaphore post succeeds; it is not expected to fail.
+ *
+ * This function increments the count of a semaphore. Any thread may call this
+ * function to increment a semaphore's count.
+ *
+ * @param[in] pSemaphore The semaphore to unlock.
+ */
 void IotSemaphore_Post(IotSemaphore_t *pSemaphore)
 {
     /* Implement this function as specified here:
      * https://docs.aws.amazon.com/freertos/latest/lib-ref/c-sdk/platform/platform_threads_function_semaphorepost.html
      */
+    struct iot_semaphore_data_s *typed;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != pSemaphore);
+    typed = pSemaphore->pointer;
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, NULL != typed->handle);
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, false == ih_in_interrupt());
+
+    DEBUG_MSG("-s-s-s %s using semaphore at %p\r\n", __FUNCTION__, typed);
+
+    IH_ASSERT(IH_ERR_LEVEL_ERROR, pdTRUE == xSemaphoreGive(typed->handle));
 }
 
-/*-----------------------------------------------------------*/
